e2973b8465b08637bd12af8c84154ffe30590c03
xy-lin <linxinyu701@hotmail.co.uk>
2017-05-18 20:02:08 +0100

    dump

--- piCam.py
+++ piCam.py
@@ -10,6 +10,7 @@ import sys
 os.system('sudo modprobe w1-gpio')
 os.system('sudo modprobe w1-therm')
  
+
 base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
